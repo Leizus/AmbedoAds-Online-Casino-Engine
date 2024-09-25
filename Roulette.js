fed311b4bb2c29c65614d52011d925b33e711667
Leizus <molinamuriel.d@gmail.com>
2024-09-25 19:27:57 +0800

    js update

---
+++ Roulette.js
@@ -0,0 +1,46 @@
+const topRowClick = document.querySelectorAll(".topRow"); //selects the Top Row
+const topClick = document.getElementById("top2to1"); // 2 to 1 button
+const midRowClick = document.querySelectorAll(".midRow"); //selects the Middle Row
+const midClick = document.getElementById("mid2to1"); // 2 to 1 button
+const botRowClick = document.querySelectorAll(".bottomRow"); //selects the Bottom Row
+const botClick = document.getElementById("bot2to1"); // 2 to 1 button
+
+
+
+
+topClick.addEventListener("mouseover", event => {
+    topRowClick.forEach((e) => {
+        e.style.border.color = "orange";
+        e.style.backgroundColor = "white";
+    });
+});
+
+topClick.addEventListener("mouseout", event => {
+    topRowClick.forEach((e) => {
+        e.style.backgroundColor = "green";
+    });
+});
+
+midClick.addEventListener("mouseover", event => {
+    midRowClick.forEach((e) => {
+        e.style.backgroundColor = "white";
+    });
+});
+
+midClick.addEventListener("mouseout", event => {
+    midRowClick.forEach((e) => {
+        e.style.backgroundColor = "green";
+    });
+});
+
+botClick.addEventListener("mouseover", event => {
+    botRowClick.forEach((e) => {
+        e.style.backgroundColor = "white";
+    });
+});
+
+botClick.addEventListener("mouseout", event => {
+    botRowClick.forEach((e) => {
+        e.style.backgroundColor = "green";
+    });
+});
