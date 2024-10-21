08c4fa39fa54b6bc9465c39d526dd88890e71aae
Leizus <molinamuriel.d@gmail.com>
2024-10-21 15:33:37 +0800

    Roulette Bet Update

--- Roulette.js
+++ Roulette.js
@@ -11,9 +11,27 @@ const sTwelve = document.querySelectorAll(".s12"); //selects second twelve
 const sTwelveMouse = document.getElementById("2ndTwelve");
 const tTwelve = document.querySelectorAll(".t12"); //selects third twelve
 const tTwelveMouse = document.getElementById("3rdTwelve");
+//1to18
+const oneToEselect = document.querySelectorAll(".ote");
+const oneToEighteen = document.getElementById("oneTo18");
+//19to36
+const nineteenToTselect = document.querySelectorAll(".ntt");
+const nineteenToThirtysix = document.getElementById("nineteenTo36");
+//even
+const evenNoSelect = document.querySelectorAll(".evenNo");
+const evenNumbered = document.getElementById("evenBtn");
+//odd
+const oddNoSelect = document.querySelectorAll(".oddNo");
+const oddNumbered = document.getElementById("oddBtn");
+//red
+const redSelect = document.querySelectorAll(".red");
+const redNums = document.getElementById("redBtn");
+//black
+const blkSelect = document.querySelectorAll(".black");
+const blkNums = document.getElementById("blkBtn");
 
 //betClicks
-var Three;
+var betChoice;
 const betThree = document.getElementById("t3");
 const betSix = document.getElementById("t6");
 const betNine = document.getElementById("t9");
@@ -21,87 +39,233 @@ const betTwelve = document.getElementById("t12");
 const betFifteen = document.getElementById("t15");
 const betEighteen = document.getElementById("t28");
 
+
+topClick.addEventListener("click", function(){
+  console.log("YOU CLICKED ON TOP ROW!!");
+  window.alert("YOU CLICKED ON TOP ROW!!");
+  betChoice = topRowNum;
+  console.log("your choice is " + betChoice);
+});
+
+midClick.addEventListener("click", function(){
+  console.log("YOU CLICKED ON MID ROW!!");
+  window.alert("YOU CLICKED ON MID ROW!!");
+  betChoice = midRowNum;
+  console.log("your choice is " + betChoice);
+});
+
+botClick.addEventListener("click", function(){
+  console.log("YOU CLICKED ON BOTTOM ROW!!");
+  window.alert("YOU CLICKED ON BOTTOM ROW!!");
+  betChoice = botRowNum;
+  console.log("your choice is " + betChoice);
+});
+
+fTwelveMouse.addEventListener("click", function(){
+  console.log("YOU CLICKED ON FIRST TWELVES!!");
+  window.alert("YOU CLICKED ON FIRST TWELVES!!");
+  betChoice = first12Num;
+  console.log("your choice is " + betChoice);
+});
+
+sTwelveMouse.addEventListener("click", function(){
+  console.log("YOU CLICKED ON SECOND TWELVES!!");
+  window.alert("YOU CLICKED ON SECOND TWELVES!!");
+  betChoice = second12Num;
+  console.log("your choice is " + betChoice);
+});
+
+tTwelveMouse.addEventListener("click", function(){
+  console.log("YOU CLICKED ON THIRD TWELVES!!");
+  window.alert("YOU CLICKED ON THIRD TWELVES!!");
+  betChoice = third12Num;
+  console.log("your choice is " + betChoice);
+});
+
+oneToEighteen.addEventListener("click", function(){
+  console.log("YOU CLICKED ON 1 - 18!!");
+  window.alert("YOU CLICKED ON 1 - 18!!");
+  betChoice = oneTo18Num;
+  console.log("your choice is " + betChoice);
+});
+
+redNums.addEventListener("click", function(){
+  console.log("YOU CLICKED ON RED !!");
+  window.alert("YOU CLICKED ON RED !!");
+  betChoice = redNum;
+  console.log("your choice is " + betChoice);
+});
+
+blkNums.addEventListener("click", function(){
+  console.log("YOU CLICKED ON BLACK !!");
+  window.alert("YOU CLICKED ON BLACK !!");
+  betChoice = blkNum;
+  console.log("your choice is " + betChoice);
+});
+
 betThree.addEventListener("click", function(){
   console.log("YOU CLICKED ON 3!!");
   window.alert("YOU CLICKED ON 3!!");
+  betChoice = 3;
+  console.log("your choice is " + betChoice);
 });
+//betClicks
+
 
 //twelves
 fTwelveMouse.addEventListener("mouseover", event => {
   fTwelve.forEach((e) => {
-      e.style.setProperty('transform', 'scale(1.09)');
+      e.style.filter = "brightness(200%)";
   });
 });
 
 fTwelveMouse.addEventListener("mouseout", event => {
   fTwelve.forEach((e) => {
-      e.style.setProperty('transform', 'scale(1)');
+      e.style.filter = "brightness(100%)";
   });
 });
 
 sTwelveMouse.addEventListener("mouseover", event => {
   sTwelve.forEach((e) => {
-      e.style.setProperty('transform', 'scale(1.09)');
+      e.style.filter = "brightness(200%)";
   });
 });
 
 sTwelveMouse.addEventListener("mouseout", event => {
   sTwelve.forEach((e) => {
-      e.style.setProperty('transform', 'scale(1)');
+      e.style.filter = "brightness(100%)";
   });
 });
 
 tTwelveMouse.addEventListener("mouseover", event => {
   tTwelve.forEach((e) => {
-      e.style.setProperty('transform', 'scale(1.09)');
+      e.style.filter = "brightness(200%)";
   });
 });
 
 tTwelveMouse.addEventListener("mouseout", event => {
   tTwelve.forEach((e) => {
-      e.style.setProperty('transform', 'scale(1)');
+      e.style.filter = "brightness(100%)";
   });
 });
 //--
+
+//1to18
+oneToEighteen.addEventListener("mouseover", event => {
+  oneToEselect.forEach((e) => {
+      e.style.filter = "brightness(200%)";
+  });
+});
+
+oneToEighteen.addEventListener("mouseout", event => {
+  oneToEselect.forEach((e) => {
+      e.style.filter = "brightness(100%)";
+  });
+});
+
+//19to36
+nineteenToThirtysix.addEventListener("mouseover", event => {
+  nineteenToTselect.forEach((e) => {
+      e.style.filter = "brightness(200%)";
+  });
+});
+
+nineteenToThirtysix.addEventListener("mouseout", event => {
+  nineteenToTselect.forEach((e) => {
+      e.style.filter = "brightness(100%)";
+  });
+});
+
+//even
+evenNumbered.addEventListener("mouseover", event => {
+  evenNoSelect.forEach((e) => {
+      e.style.filter = "brightness(450%)";
+  });
+});
+
+evenNumbered.addEventListener("mouseout", event => {
+  evenNoSelect.forEach((e) => {
+      e.style.filter = "brightness(100%)";
+  });
+});
+
+//odd
+oddNumbered.addEventListener("mouseover", event => {
+  oddNoSelect.forEach((e) => {
+      e.style.filter = "brightness(450%)";
+  });
+});
+
+oddNumbered.addEventListener("mouseout", event => {
+  oddNoSelect.forEach((e) => {
+      e.style.filter = "brightness(100%)";
+  });
+});
+
+//red
+redNums.addEventListener("mouseover", event => {
+  redSelect.forEach((e) => {
+      e.style.filter = "brightness(450%)";
+  });
+});
+
+redNums.addEventListener("mouseout", event => {
+  redSelect.forEach((e) => {
+      e.style.filter = "brightness(100%)";
+  });
+});
+
+//black
+blkNums.addEventListener("mouseover", event => {
+  blkSelect.forEach((e) => {
+      e.style.filter = "brightness(450%)";
+  });
+});
+
+blkNums.addEventListener("mouseout", event => {
+  blkSelect.forEach((e) => {
+      e.style.filter = "brightness(100%)";
+  });
+});
 
 //top row styles
 
 topClick.addEventListener("mouseover", event => {
   topRowClick.forEach((e) => {  
-      e.style.setProperty('transform', 'scale(1.09)');
+      e.style.filter = "brightness(200%)";
       
   });
 });
 
 topClick.addEventListener("mouseout", event => {
   topRowClick.forEach((e) => {
-      e.style.setProperty('transform', 'scale(1)');
+      e.style.filter = "brightness(100%)";
   });
 });
 
 //middle row styles
 midClick.addEventListener("mouseover", event => {
   midRowClick.forEach((e) => {
-      e.style.setProperty('transform', 'scale(1.09)');    
+      e.style.filter = "brightness(200%)";    
   });
 });
 
 midClick.addEventListener("mouseout", event => {
   midRowClick.forEach((e) => {
-      e.style.setProperty('transform', 'scale(1)');
+      e.style.filter = "brightness(100%)";
   });
 });
 
 //bottom row styles
 botClick.addEventListener("mouseover", event => {
   botRowClick.forEach((e) => {
-      e.style.setProperty('transform', 'scale(1.09)');    
+      e.style.filter = "brightness(200%)";    
   });
 });
 
 botClick.addEventListener("mouseout", event => {
   botRowClick.forEach((e) => {
-      e.style.setProperty('transform', 'scale(1)');
+      e.style.filter = "brightness(100%)";
   });
 });
 //--
@@ -353,6 +517,13 @@ var isRotating = false;
 var zero = [ 0 ];
 var blkNum = [ 2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 29, 28, 31, 33, 35 ];
 var redNum = [ 1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36 ];
+var topRowNum = [ 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36 ];
+var midRowNum = [ 2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35 ];
+var botRowNum = [ 1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34 ];
+var first12Num = [ 3, 6, 9, 12, 2, 5, 8, 11, 14, 1, 4, 7, 10,];
+var second12Num = [ 15, 18, 21, 24, 14, 17, 20, 23, 13, 16, 19, 22 ];
+var third12Num = [ 27, 30, 33, 36, 26, 29, 32, 35, 25, 28, 31, 34 ];
+var oneTo18Num = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 ];
 const rouletteWheelNumbers = [
   0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26 ];
 
@@ -361,7 +532,7 @@ const getRouletteWheelNumber = index =>
 
 const getRouletteWheelColor = index => {
   const i = index >= 0 ? index % 37 : 37 - Math.abs(index % 37);
-  return i == 0 ? "green" : i % 2 == 0 ? "black" : "red";
+  return i === 0 ? "green" : i % 2 == 0 ? "black" : "red";
 };
 
 window.rouletteWheelNumbers = rouletteWheelNumbers;
@@ -377,10 +548,17 @@ function addFlipper() { //Result Card Function
   const back = mkDiv("back-face");
   flipper.appendChild(front);
   flipper.appendChild(back);
-  document.querySelector(".result").appendChild(flipper);
+  document.querySelector(".resultNum").appendChild(flipper);
   return (number, color) => {
+    if (number == null){
+      number = 0;
+      color = "green";
+    }
     flipper.classList.add("flip", color);
-    back.innerText = number;
+    setTimeout(() => {
+      flipper.style.display = "none"; //removes flipper card after Nsecs
+    }, 10000);
+    back.innerText = number;    
   };
 }
 //--
@@ -401,7 +579,7 @@ function startRotation(speed) {
   const resultColor = getRouletteWheelColor(newWheelIndex);
   (() => {
     const newRotation = currentWheelRotation + (360 / 37) * speed;
-    console.log(getRouletteWheelNumber(currentWheelIndex), "---> ", result);
+    console.log(getRouletteWheelNumber(currentWheelIndex), "---> ");
     var myAnimation = anime({
       targets: [".layer-2", ".layer-4"],
       rotate: function() {
@@ -411,9 +589,9 @@ function startRotation(speed) {
         return 5000;
       },
       loop: 1,
-      // easing: "cubicBezier(0.010, 0.990, 0.855, 1.010)",
+      easing: "cubicBezier(0.010, 0.990, 0.855, 1.010)",
       easing: `cubicBezier(${bezier.join(",")})`,
-      // easing: "cubicBezier(0.000, 1.175, 0.980, 0.990)",
+      easing: "cubicBezier(0.000, 1.175, 0.980, 0.990)",
       complete: (...args) => {
         currentWheelRotation = newRotation;
         currentWheelIndex = newWheelIndex;
@@ -448,22 +626,37 @@ function startRotation(speed) {
 
   //Betting
   function betEngine() {
-    console.log("this is the Betting portion.");
-    console.log(result);
 
-    if (blkNum.includes(result)){
-      console.log("BLACK WON");
-    }
+    setTimeout(() =>{
+      console.log("this is the Betting portion.");
+      console.log(result);
 
-    else if (redNum.includes(result)){
-      console.log("RED WON");
-    }
+      if (betChoice == null){
+        console.log("YOU DIDN'T CHOOSE A NUMBER")
+        window.alert("YOU DIDN'T CHOOSE A NUMBER")
+      }
 
-    else if (zero.includes(result)){
-      console.log("ZERO WON");
-    }
-    
-    
+      else if (!betChoice.includes(result)){
+        console.log("TRY AGAIN")
+        window.alert("TRY AGAIN")
+      }
+
+      else if (blkNum.includes(result) && betChoice.includes(result)){
+
+        console.log("YOU'RE IN CHECKING");
+        console.log("YOU WON " + result);
+        window.alert("YOU WON " + result);
+      }
+      
+      else if (redNum.includes(result) && betChoice.includes(result)){
+        console.log("YOU'RE IN CHECKING");
+        console.log("YOU WON " + result);
+        window.alert("YOU WON " + result);
+        
+      }
+
+
+    }, 5000);
   }
   //Betting
 
